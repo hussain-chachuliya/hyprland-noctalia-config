2596cdce10b29002e6201a6be923edb5c532a1ef
Hussain Chachuliya <hussain.patience@gmail.com>
2026-09-26 10:35:53 +0400

    updates (#8)
    * make brave-origin default browser
    
    * update keybindings and add mimeapps to backup defaults
    
    * remove wallpaper based noctalia theme files
    
    * Untrack noctalia color changes + add read me

--- hypr/config/variables.lua
+++ hypr/config/variables.lua
@@ -9,10 +9,11 @@ local variables = {
     -- Default apps
     TERMINAL     = "alacritty",
     FILE_MANAGER = "thunar",
-    BROWSER      = "brave",
+    BROWSER      = "brave-origin",
     EDITOR       = "geany",
     CALCULATOR   = "gnome-calculator",
     OPENCODE     = "opencode-desktop",
+    NOTES        = "obsidian",
 
     -- Monitors
     MONITOR1 = MONITOR1,
